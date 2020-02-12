e71266ee179c6e88942be6d47895c58c14b88528
admin <admin@localhost.local>
2020-02-12 17:29:47 +0800

    python runtime

--- reqreshandler/request_id.py
+++ reqreshandler/request_id.py
@@ -3,19 +3,20 @@ import uuid
 from plugin.reqres_handler_chain.reqres_handler import Handler
 from flask import Request, Response
 
+
 class RequestIdHandler(Handler):
 
-    def doBefore(self, req:Request)->Request:
+    def doBefore(self, req: Request) -> Request:
         if hasattr(self, "request_id"):
             print("old RequestIdHandler.request_id:"+str(self.request_id))
-        if hasattr(req, "X_Request_Id"):
-            print(req.X_Request_Id)
+        if hasattr(req, "X_Faas_Request_Id"):
+            print(req.X_Faas_Request_Id)
         self.request_id = uuid.uuid4()
         # request.headers.remove("X-Request-Id")
-        req.X_Request_Id= self.request_id
+        req.X_Faas_Request_Id = self.request_id
         return req
 
-    def doAfter(self, res:Response)->Response:
-        res.headers.remove("X-Request-Id")
-        res.headers.add("X-Request-Id", self.request_id)
+    def doAfter(self, res: Response) -> Response:
+        res.headers.remove("X-Faas-Request-Id")
+        res.headers.add("X-Faas-Request-Id", self.request_id)
         return res
--- reqreshandler/trace_id.py
+++ reqreshandler/trace_id.py
@@ -3,16 +3,17 @@ import uuid
 from plugin.reqres_handler_chain.reqres_handler import Handler
 from flask import Request, Response
 
+
 class TraceIdHandler(Handler):
 
-    def doBefore(self, req:Request)->Request:
-        self.trace_id = req.headers.get("X-Trace-Id", req.headers.get("Trace-Id", uuid.uuid4()))
-        req.X_Trace_Id = self.trace_id
+    def doBefore(self, req: Request) -> Request:
+        self.trace_id = req.headers.get("X-Faas-Trace-Id", uuid.uuid4())
+        req.X_Faas_Trace_Id = self.trace_id
         return req
 
-    def doAfter(self, res:Response)->Response:
+    def doAfter(self, res: Response) -> Response:
         # res.headers.remove("Trace-Id")
         # res.headers.remove("X-Trace-Id")
         # res.headers.add("Trace-Id", self.trace_id)
-        res.headers.add("X-Trace-Id", self.trace_id)
+        res.headers.add("X-Faas-Trace-Id", self.trace_id)
         return res

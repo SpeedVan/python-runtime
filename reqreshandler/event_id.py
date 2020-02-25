49325626e4efc0dc4b18ddc21df3a2ccffdc2c05
admin <admin@localhost.local>
2020-02-25 15:16:31 +0800

    change header

--- reqreshandler/event_id.py
+++ reqreshandler/event_id.py
@@ -7,11 +7,12 @@ from flask import Request, Response
 class EventIdHandler(Handler):
 
     def doBefore(self, req: Request) -> Request:
-        self.event_id = req.headers.get("X-Faas-Event-Id", uuid.uuid4())
-        req.X_Faas_Event_Id = self.event_id
+        self.X_System_Event_Id = req.headers.get(
+            "X-System-Event-Id", uuid.uuid4())
+        req.X_System_Event_Id = self.X_System_Event_Id
         return req
 
     def doAfter(self, res: Response) -> Response:
-        res.headers.remove("X-Faas-Event-Id")
-        res.headers.add("X-Faas-Event-Id", self.event_id)
+        res.headers.remove("X-System-Event-Id")
+        res.headers.add("X-System-Event-Id", self.X_System_Event_Id)
         return res

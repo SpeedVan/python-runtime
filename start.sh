20b73d1cd2874becb0042c978a84aa466e70b6a5
SpeedVan <admin@localhost.local>
2020-05-28 17:24:27 +0800

    fix

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE-$0}")"; pwd)
 
-PROXY_BUFFERSIZE=50python ${CUR_DIR}/app.py "{\"bind\":\"0.0.0.0:5000\", \"ENTRYPOINT\": \"/Users/admin/projects/go/src/github.com/SpeedVan/python-runtime/test_func/func_1.func\", \"FREEBACK_CODE\":\"aaaaa\"}"
+python3 ${CUR_DIR}/app.py $1

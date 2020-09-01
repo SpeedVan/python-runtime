9a0c840a9f5f4ef2999aa7583a1cd81efd3e8d82
SpeedVan <admin@localhost.local>
2020-09-01 19:30:33 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 CUR_DIR=$(cd "$(dirname "${BASH_SOURCE-$0}")"; pwd)
 
-# args=$1
-# python3 ${CUR_DIR}/app.py ${args}
-export PROXY_BUFFERSIZE=50
-args="{\"bind\":\"0.0.0.0:5000\",\"ENTRYPOINT\":\"/Users/admin/projects/go/src/github.com/SpeedVan/python-runtime/test_func/func_1.func\",\"FREEBACK_CODE\":\"aaaaa\"}"
+args=$1
+python3 ${CUR_DIR}/app.py ${args}
+# export PROXY_BUFFERSIZE=50
+# args="{\"bind\":\"0.0.0.0:5000\",\"ENTRYPOINT\":\"/Users/admin/projects/go/src/github.com/SpeedVan/python-runtime/test_func/func_1.func\",\"FREEBACK_CODE\":\"aaaaa\"}"
 
-/Users/admin/projects/go/src/github.com/SpeedVan/python-runtime/bin/python3 ${CUR_DIR}/app.py ${args}
+# /Users/admin/projects/go/src/github.com/SpeedVan/python-runtime/bin/python3 ${CUR_DIR}/app.py ${args}
